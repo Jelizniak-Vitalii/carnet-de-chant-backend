4749972cb6578ed70a720f0f09005192870133fa
vitalii <vitaly.angular.bsx@gmail.com>
2022-01-31 00:57:40 +0200

    add error

--- routes/get-chants-by-name.js
+++ routes/get-chants-by-name.js
@@ -3,17 +3,15 @@ const routerGetChantsByName = require('express').Router();
 const Chants = require('../models/chant.model');
 
 routerGetChantsByName.post('/getChantsByName', (req, res) => {
-    const {name} = req.body;
+    const { name } = req.body;
     let regex = RegExp(".*" + name.toUpperCase() + ".*");
     Chants.find({name: regex}, (err, chant) => {
-        if (!chant) {
-            return res.status(200).send({response: 'No songs found with this name'});
-        };
-        res.status(200).send(chant);
+        if (err) return res.send(err);
 
-        if (err) {
-            res.send(err);
-            console.log(err)
+        if (chant) {
+            return res.status(200).send(chant);
+        } else {
+            return res.status(200).send({response: 'No songs found with this name'});
         }
     });
 });
